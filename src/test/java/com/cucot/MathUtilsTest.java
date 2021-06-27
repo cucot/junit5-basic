0e8d419d0c043f9cfb66b63bce828b540ee02b43
cucot <phucnn.work@gmail.com>
2021-06-27 12:33:26 +0700

    fail and disabled for case in progress

--- src/test/java/com/cucot/MathUtilsTest.java
+++ src/test/java/com/cucot/MathUtilsTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.fail;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class MathUtilsTest {
@@ -53,6 +55,13 @@ class MathUtilsTest {
     @DisplayName("Add with underflow")
     void addExceedMinInteger() {
         assertThrows(RuntimeException.class, () -> mathUtils.add(Integer.MIN_VALUE, -1), "Result < Integer.MIN_VALUE will lead to overflow, cause unintended incorrect calculation");
+    }
+
+    @Test
+    @DisplayName("Fail this test")
+    @Disabled
+    void failThis() {
+        fail("This test should not be run");
     }
 
 }
